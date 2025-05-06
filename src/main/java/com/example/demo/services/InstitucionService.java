ffa51c3faa7e795a403a3eee115f8d58d2af9c45
JorgeMarles <jamarlesf@gmail.com>
2025-05-05 21:06:12 -0500

    chore: validations for crear Institucion

--- src/main/java/com/example/demo/services/InstitucionService.java
+++ src/main/java/com/example/demo/services/InstitucionService.java
@@ -7,12 +7,16 @@ import org.springframework.stereotype.Service;
 import com.example.demo.exceptions.ResourceNotFoundException;
 import com.example.demo.DTO.response.InstitucionDTO;
 import com.example.demo.entities.Institucion;
+import com.example.demo.entities.TipoInstitucion;
 import com.example.demo.repositories.InstitucionRepository;
 
 @Service
 public class InstitucionService {
 	@Autowired
 	private InstitucionRepository institucionRepository;
+
+	@Autowired
+	private TipoInstitucionService tipoInstitucionService;
 
 	@Transactional
 	public InstitucionDTO crear(Map<String, Object> institucionDTO) {
@@ -26,7 +30,17 @@ public class InstitucionService {
 			throw new IllegalArgumentException("Tipo de Institucion es obligatorio.");
 		}
 
+		if(!(institucionDTO.get("id_tipo_institucion") instanceof Long)) {
+			throw new IllegalArgumentException("El id_tipo_institucion debe ser un número.");
+		}
+		if(!(institucionDTO.get("nombre") instanceof String)) {
+			throw new IllegalArgumentException("El nombre debe ser un texto.");
+		}
+
+		TipoInstitucion tipoInstitucion = tipoInstitucionService.buscarPorIdEntidad((Long) institucionDTO.get("id_tipo_institucion"));
+
 		institucion.setNombre((String) institucionDTO.get("nombre"));
+		institucion.setTipoInstitucion(tipoInstitucion);
 		institucion = institucionRepository.save(institucion);
 
 		InstitucionDTO institucionResponse = new InstitucionDTO();
