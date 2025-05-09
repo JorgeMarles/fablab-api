5a832d4a4551eea2622ba4598bd79ac7c004d20c
JorgeMarles <jamarlesf@gmail.com>
2025-05-08 22:59:22 -0500

    feat: GET ofertas/ for all 3 roles

---
+++ src/main/java/com/example/demo/controllers/OfertaFormacionController.java
@@ -0,0 +1,42 @@
+package com.example.demo.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.DTO.response.OfertaItemDTO;
+import com.example.demo.services.OfertaFormacionService;
+
+@RestController
+@RequestMapping("/ofertas")
+public class OfertaFormacionController {
+    
+    @Autowired
+    private OfertaFormacionService ofertaFormacionService;
+
+    @GetMapping("/")
+    //@Preauthorize admin
+    public ResponseEntity<List<OfertaItemDTO>> listarTodos(){
+        return ResponseEntity.ok().body(ofertaFormacionService.listarTodos());
+    }
+
+    @GetMapping("/instructor")
+    //preauth instructor
+    public ResponseEntity<List<OfertaItemDTO>> listarInstructor(){
+        //TODO: Sacar del userdetails del authorization
+        Long id = 3L;
+        return ResponseEntity.ok().body(ofertaFormacionService.listarInstructor(id)); 
+    }
+
+    @GetMapping("/participante")
+    //preauth instructor
+    public ResponseEntity<List<OfertaItemDTO>> listarParticipante(){
+        //TODO: Sacar del userdetails del authorization
+        Long id = 3L;
+        return ResponseEntity.ok().body(ofertaFormacionService.listarParticipante(id)); 
+    }
+}
