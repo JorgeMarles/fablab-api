dca3eb7fdb7f78f204da4a28b6356eb7eb53f916
JorgeMarles <jamarlesf@gmail.com>
2025-05-23 22:47:28 -0500

    feat: find by search participante

--- src/main/java/com/example/demo/controllers/ParticipanteController.java
+++ src/main/java/com/example/demo/controllers/ParticipanteController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controllers;
 
 import java.net.URI;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +27,16 @@ public class ParticipanteController {
     public ResponseEntity<DatosPersonalesResponseDTO> crearParticipante(@RequestBody DatosPersonalesDTO participanteDTO) {
         DatosPersonalesResponseDTO creado = usuarioService.crearParticipante(participanteDTO, null, true);
         return ResponseEntity.created(URI.create("/participantes/" + creado.getId())).body(creado);
+    }
+
+    @GetMapping("/{id}/")
+    public ResponseEntity<DatosPersonalesResponseDTO> obtenerParticipantePorId(@PathVariable Long id) {
+        DatosPersonalesResponseDTO dto = usuarioService.obtenerPorId(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping("/search/")
+    public ResponseEntity<List<DatosPersonalesResponseDTO>> obtenerParticipantePorEmail(@RequestBody String q) {
+        return ResponseEntity.ok(usuarioService.buscar(q));
     }
 }
